aad9f0b45925e5eac45686b5132e8b8f7966ea05
motecshine <shinemotec@gmail.com>
2018-01-21 18:17:15 +0800

    goroutines

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -9,11 +9,11 @@ import (
 func Loop(Channel chan int) {
 	var m *sync.Mutex
 	m = new(sync.Mutex)
-	defer m.Unlock()
 	m.Lock()
 	for i := 0; i < 10; i++ {
 		time.Sleep(3 * time.Second)
 		fmt.Printf("%d\n", i)
 	}
+	m.Unlock()
 	Channel <- 0 // 数据流入
 }
